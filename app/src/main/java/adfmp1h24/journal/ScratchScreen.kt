2f70b552cd19e75423a3c03d3d4419cb1ff1b348
abbadonref <jokerokerok79@gmail.com>
2024-03-20 23:32:31 +0300

    Sidebar fix

--- app/src/main/java/adfmp1h24/journal/ScratchScreen.kt
+++ app/src/main/java/adfmp1h24/journal/ScratchScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
--- app/src/main/java/adfmp1h24/journal/Sidebar.kt
+++ app/src/main/java/adfmp1h24/journal/Sidebar.kt
@@ -4,21 +4,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.CoroutineScope
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 @Composable
@@ -26,45 +23,47 @@ fun Sidebar() {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     var screen by remember { mutableStateOf(ScreenType.Library) }
     val scope = rememberCoroutineScope()
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        drawerContent = {
-            ModalDrawerSheet {
-                Row(
-                    modifier = Modifier.clickable{
-                        screen = ScreenType.Library
-                        scope.launch { drawerState.close() }
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr ) {
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Row(
+                        modifier = Modifier.clickable{
+                            screen = ScreenType.Library
+                            scope.launch { drawerState.close() }
+                        }
+                    ) {
+                        Text(text = "Library", fontSize = 20.sp, modifier = Modifier.padding(20.dp))
                     }
-                ) {
-                    Text(text = "Library")
-                }
-                Row(
-                    modifier = Modifier.clickable{
-                        screen = ScreenType.Scratch
+                    Row(
+                        modifier = Modifier.clickable{
+                            screen = ScreenType.Scratch
+                        }
+                    ) {
+                        Text(text = "Add new scratch", fontSize = 20.sp, modifier = Modifier.padding(20.dp))
                     }
-                ) {
-                    Text(text = "Add new scratch")
-                }
-                Row(
-                    modifier = Modifier.clickable{
-                        screen = ScreenType.About
-                        scope.launch { drawerState.close() }
+                    Row(
+                        modifier = Modifier.clickable{
+                            screen = ScreenType.About
+                            scope.launch { drawerState.close() }
+                        }
+                    ) {
+                        Text(text = "About", fontSize = 20.sp, modifier = Modifier.padding(20.dp))
                     }
-                ) {
-                    Text(text = "About")
                 }
-            }
-        },
-    ) {
-        Column(modifier = Modifier.padding(15.dp)) {
-            when (screen) {
-                ScreenType.Library -> MainScreen(
-                    onNavigate = { screen = it },
-                    sidebarState = drawerState,
-                    scope = scope
-                )
-                ScreenType.Scratch -> ScratchScreen(onNavigate = { screen = it })
-                ScreenType.About -> AboutScreen(onNavigate = { screen = it })
+            },
+        ) {
+            Column(modifier = Modifier.padding(15.dp)) {
+                when (screen) {
+                    ScreenType.Library -> MainScreen(
+                        onNavigate = { screen = it },
+                        sidebarState = drawerState,
+                        scope = scope
+                    )
+                    ScreenType.Scratch -> ScratchScreen(onNavigate = { screen = it })
+                    ScreenType.About -> AboutScreen(onNavigate = { screen = it })
+                }
             }
         }
     }

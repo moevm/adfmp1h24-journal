20f3e2610f96799cd01c98840c06a5a608971c35
abbadonref <jokerokerok79@gmail.com>
2024-03-27 17:43:42 +0300

    fix Filter

--- app/src/main/java/adfmp1h24/journal/CustomDatePicker.kt
+++ app/src/main/java/adfmp1h24/journal/CustomDatePicker.kt
@@ -1,18 +1,24 @@
 package adfmp1h24.journal
 
+import adfmp1h24.journal.ui.theme.Primary
+import adfmp1h24.journal.ui.theme.White
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
 import com.maxkeppeler.sheets.calendar.CalendarDialog
 import com.maxkeppeler.sheets.calendar.models.CalendarConfig
@@ -27,17 +33,22 @@ import java.time.format.DateTimeFormatter
 fun CustomDatePicker(date: LocalDate, callback: (LocalDate) -> Unit) {
     var pickedDate by remember { mutableStateOf(date) }
     var isOpened by remember { mutableStateOf(false) }
-    TextField(
+    OutlinedTextField(
         value = pickedDate.format(DateTimeFormatter.ISO_DATE) ,
         onValueChange = {},
         enabled = false,
         modifier = Modifier.fillMaxWidth().clickable {
             isOpened = true
         },
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = White,
+            focusedIndicatorColor = Primary,
+            unfocusedIndicatorColor = Primary
+        )
     )
     if (isOpened) {
         CalendarDialog(
-            state = rememberUseCaseState(visible = true, true, onCloseRequest = { isOpened = false} ),
+            state = rememberUseCaseState(visible = true, true, onCloseRequest = { isOpened = false } ),
             config = CalendarConfig(
                 yearSelection = true,
                 style = CalendarStyle.MONTH,
--- app/src/main/java/adfmp1h24/journal/Filters.kt
+++ app/src/main/java/adfmp1h24/journal/Filters.kt
@@ -8,9 +8,12 @@ import adfmp1h24.journal.ui.theme.White
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Place
@@ -23,7 +26,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -40,34 +45,48 @@ fun Filters(isScratch: Boolean = false, fromDateChanged: (String) -> Unit, toDat
         border = BorderStroke(1.dp, Primary),
         ) {
         if (!isScratch) {
-            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp).padding(top = 10.dp)) {
+            Row(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
                 Icon(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .align(Alignment.CenterVertically),
                     imageVector = Icons.Default.DateRange,
                     contentDescription = null,
                     tint = Primary
                 )
                 Row(){
-                    Text("From: ")
+                    Text("From: ", modifier = Modifier.width(50.dp)
+                        .fillMaxHeight()
+                        .align(Alignment.CenterVertically),
+                    )
                     CustomDatePicker(
                         date = LocalDate.parse(fromDate, DateTimeFormatter.ISO_DATE)
                     ) { fromDate = it.format(DateTimeFormatter.ISO_DATE); fromDateChanged(fromDate) }
                 }
             }
-            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp)) {
+            Row(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
                 Icon(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .align(Alignment.CenterVertically),
                     imageVector = Icons.Default.DateRange,
                     contentDescription = null,
                     tint = Primary
                 )
                 Row(){
-                    Text("To: ")
+                    Text("To: ",
+                        modifier = Modifier.width(50.dp)
+                            .fillMaxHeight()
+                            .align(Alignment.CenterVertically),
+                        textAlign = TextAlign.Center
+                    )
                     CustomDatePicker(
                         date = LocalDate.parse(toDate, DateTimeFormatter.ISO_DATE)
                     ) { toDate = it.format(DateTimeFormatter.ISO_DATE); toDateChanged(toDate) }
                 }
             }
         } else {
-            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp).padding(top = 10.dp)) {
+            Row(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
                 Icon(
                     imageVector = Icons.Default.DateRange,
                     contentDescription = null,
@@ -77,7 +96,7 @@ fun Filters(isScratch: Boolean = false, fromDateChanged: (String) -> Unit, toDat
             }
         }
         /* TODO: эм... ну мы не успеем запилить поиск по месту, при всём желании */
-        Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp).padding(bottom = 10.dp)) {
+        /* Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp).padding(bottom = 10.dp)) {
             Icon(
                 imageVector = Icons.Default.Place,
                 contentDescription = null,
@@ -85,5 +104,6 @@ fun Filters(isScratch: Boolean = false, fromDateChanged: (String) -> Unit, toDat
             )
             Text(text = "Place: placeholder")
         }
+         */
     }
 }
